3971e55994098da51482ab321f06f6d1ceb01328
satish <92359639+satis12@users.noreply.github.com>
2023-09-09 12:12:00 -0400

    gym management system files

---
+++ authapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from authapp.models import Contact,MembershipPlan,Enrollment,Trainer,Attendance
+# Register your models here.
+
+
+admin.site.register(Contact)
+admin.site.register(MembershipPlan)
+admin.site.register(Enrollment)
+admin.site.register(Trainer)
+admin.site.register(Attendance)
+
